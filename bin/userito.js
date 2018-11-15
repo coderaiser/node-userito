0876aa2d554c23057356bb3d1c2d1b83a56162b1
coderaiser <coderaiser@cloudcmd.io>
2018-11-15 14:44:09 +0200

    feature(userito) es2015-ify

--- bin/userito.js
+++ bin/userito.js
@@ -1,92 +1,88 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
-    
-    var express     = require('express'),
-        bodyParser  = require('body-parser'),
-        info        = require('../package'),
-        app         = express(),
-        
-        DB          = process.env.USERITO_DB,
-        userito     = require('..')({
-            type: DB ? 'db' : 'file',
-            db: DB
-        }),
-        
-        PORT        = process.env.USERITO_PORT || 3000,
-        
-        server;
-    
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    
-    app.get('/', function(req, res) {
-        res.send({
-            name: info.name,
-            version: info.version,
-            api: {
-                'GET /users': 'get all users',
-                'GET /user/:username': 'get user with :username',
-                'PUT /user/:username': 'modify user with :username',
-                'POST /user': 'create user',
-                'DELETE /user': 'remove user'
-            }
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const info = require('../package');
+const app = express();
+
+const db = process.env.USERITO_DB;
+const userito = require('..')({
+    type: db ? 'db' : 'file',
+    db,
+});
+
+const PORT = process.env.USERITO_PORT || 3000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.send({
+        name: info.name,
+        version: info.version,
+        api: {
+            'GET /users': 'get all users',
+            'GET /user/:username': 'get user with :username',
+            'PUT /user/:username': 'modify user with :username',
+            'POST /user': 'create user',
+            'DELETE /user': 'remove user'
+        }
+    });
+});
+
+app.get('/users', (req, res) => {
+    userito.all(send(res));
+});
+
+app.get('/user/:username', (req, res) => {
+    const username = req.params.username;
+    userito.get(username, send(res));
+});
+
+app.put('/user/:username', (req, res) => {
+    const username = req.params.username;
+    userito.modify(username, req.body, send(res));
+});
+
+app.post('/user', (req, res) => {
+    userito.create(req.body, send(res));
+});
+
+app.delete('/user/:username', (req, res) => {
+    const username = req.params.username;
+    userito.remove(username, send(res));
+});
+
+app.use('*', (req, res) => {
+    res
+        .status(500)
+        .json({
+            message: 'api not defined'
         });
-    });
+});
+
+function send(res) {
+    return (error, data, appError) => {
+        if (data)
+            res.send(data);
+        else if (error)
+            res
+                .status(500)
+                .send(error);
+        else
+            res
+                .status(400)
+                .send(appError);
+    };
+}
+
+const server = app.listen(PORT, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('%s v%s listening at http://%s:%s', info.name, info.version, host, port);
+});
     
-    app.get('/users', function (req, res) {
-        userito.all(send(res));
-    });
-    
-    app.get('/user/:username', function(req, res) {
-        var username = req.params.username;
-        userito.get(username, send(res));
-    });
-    
-    app.put('/user/:username', function(req, res) {
-        var username = req.params.username;
-        userito.modify(username, req.body, send(res));
-    });
-    
-    app.post('/user', function(req, res) {
-        userito.create(req.body, send(res));
-    });
-    
-    app.delete('/user/:username', function(req, res) {
-        var username = req.params.username;
-        userito.remove(username, send(res));
-    });
-    
-    app.use('*', function(req, res) {
-        res
-            .status(500)
-            .json({
-                message: 'api not defined'
-            });
-    });
-    
-    function send(res) {
-        return function(error, data, appError) {
-            if (data)
-                res.send(data);
-            else if (error)
-                res
-                    .status(500)
-                    .send(error);
-            else
-                res
-                    .status(400)
-                    .send(appError);
-        };
-    }
-    
-    server = app.listen(PORT, function () {
-        var host = server.address().address;
-        var port = server.address().port;
-    
-        console.log('%s v%s listening at http://%s:%s', info.name, info.version, host, port);
-    });
-    
-})();
