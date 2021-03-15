b5d858f916dc3b23c320dfdfefa6d6cad040d33c
coderaiser <coderaiser@cloudcmd.io>
2021-03-15 18:21:07 +0200

    feature(userito) drop support of node < 14

--- .madrun.mjs
+++ .madrun.mjs
@@ -5,5 +5,8 @@ export default {
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
+    'test': async () => `tape 'test/*.js' '{bin,lib}/**/*.spec.{js,mjs}'`,
+    'coverage': async () => `c8 ${await run('test')}`,
+    'report': () => 'c8 report --reporter=lcov',
 };
 
